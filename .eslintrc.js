e02c6b3d005a2d90b3937b1a39da78ae580072df
김성현 <921126sh@thewc.co.kr>
2021-06-24 16:43:41 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    root: true,
+    extends: '@react-native-community',
+    rules: {
+        indent: ['error', 4],
+        'prettier/prettier': ['error', {tabWidth: 4}],
+    },
+};
